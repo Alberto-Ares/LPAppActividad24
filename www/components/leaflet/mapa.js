32a9ffacbdf92ae0bfae72d681fdd5cb42f85e1c
Alberto-Ares <jose.ares101@alu.ulpgc.es>
2022-07-26 05:39:11 +0900

    Project's Initial Commit

---
+++ www/components/leaflet/mapa.js
@@ -0,0 +1,10 @@
+// This is a JavaScript file
+// <script>
+// var map = L.map('map').fitWorld();
+// var map = L.map('map').setView([28.10071074776168, -15.414998088476695], 14);
+
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     maxZoom: 19,
+//     attribution: '© OpenStreetMap'
+// }).addTo(map);
+// </script>
